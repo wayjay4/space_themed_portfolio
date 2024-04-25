9cb9d97591f349ea08675f7075baa2d4a73c07a8
Waylon Dixon <wd@signingdaysports.com>
2024-04-25 00:48:59 -0700

    added sliding in components, utils, and css classes

--- README.md
+++ README.md
@@ -39,4 +39,5 @@ Check out our [Next.js deployment documentation](https://nextjs.org/docs/deploym
 ## ran the following:
 ````
 npm install --save-dev framer-motion three react-intersection-observer @react-three/drei @react-three/fiber
+npm install @heroicons/react
 ````
--- components/main/Hero.tsx
+++ components/main/Hero.tsx
@@ -1,4 +1,7 @@
+"use client";
+
 import React from 'react';
+import HeroContent from "@/components/sub/HeroContent";
 
 const Hero = () => {
     return (
@@ -11,6 +14,7 @@ const Hero = () => {
             >
                 <source src="/assets/blackhole.webm" type="video/webm" />
             </video>
+            <HeroContent />
         </div>
     );
 };
