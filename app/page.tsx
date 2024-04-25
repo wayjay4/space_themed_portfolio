b4a8da3873227ff90006ee14e1d9de1af42d3a0b
Waylon Dixon <wd@signingdaysports.com>
2024-04-25 16:01:02 -0700

    added in encription component

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import Hero from "@/components/main/Hero";
 import Skills from "@/components/main/Skills";
+import Encryption from "@/components/main/Encryption";
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
           <div className="flex flex-col gap-20">
               <Hero />
               <Skills />
+              <Encryption />
           </div>
       </main>
   );
