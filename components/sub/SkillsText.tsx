35a47351ef3307303a40aa4f1da63d7bcebcce2e
Waylon Dixon <wd@signingdaysports.com>
2024-04-25 15:20:17 -0700

    fixed animation that was not working

--- components/sub/SkillsText.tsx
+++ components/sub/SkillsText.tsx
@@ -7,31 +7,37 @@ import {SparklesIcon} from "@heroicons/react/24/solid";
 
 const SkillsText = () => {
     return (
-        <div className="w-full h-full flex flex-col items-center justify-center">
-            <motion.div
-                variants={slideInFromTop()}
-                className="Welcome-box py-[8px] px-[10px] border border-[#7042f88b] opacity-[0.9]"
-            >
-                <SparklesIcon className="text-[#b49bff] mr-[10px] h-5 w-5" />
-                <h1 className="Welcome-text text-[13px]">
-                    Think better with Nextjs 14
-                </h1>
-            </motion.div>
+        <motion.div
+            initial="hidden"
+            animate="visible"
+            className="flex flex-row items-center justify-center px-20 mt-40 w-full z-[20]"
+        >
+            <div className="w-full h-full flex flex-col items-center justify-center">
+                <motion.div
+                    variants={slideInFromTop()}
+                    className="Welcome-box py-[8px] px-[10px] border border-[#7042f88b] opacity-[0.9]"
+                >
+                    <SparklesIcon className="text-[#b49bff] mr-[10px] h-5 w-5" />
+                    <h1 className="Welcome-text text-[13px]">
+                        Think better with Nextjs 14
+                    </h1>
+                </motion.div>
 
-            <motion.div
-                variants={slideInFromLeft(0.5)}
-                className="text-[30px] text-white font-medium mt-[10px] text-center mb-[15px]"
-            >
-                Making apps with modern technologies
-            </motion.div>
+                <motion.div
+                    variants={slideInFromLeft(0.5)}
+                    className="text-[30px] text-white font-medium mt-[10px] text-center mb-[15px]"
+                >
+                    Making apps with modern technologies
+                </motion.div>
 
-            <motion.div
-                variants={slideInFromRight(0.5)}
-                className="cursive text-[20px] text-gray-200 mb-10 mt-[10px] text-center"
-            >
-                Never miss a test, deadline, or idea
-            </motion.div>
-        </div>
+                <motion.div
+                    variants={slideInFromRight(0.5)}
+                    className="cursive text-[20px] text-gray-200 mb-10 mt-[10px] text-center"
+                >
+                    Never miss a test, deadline, or idea
+                </motion.div>
+            </div>
+        </motion.div>
     );
 };
 
