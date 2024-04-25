6cdfe4f4cc3cf08f5b880d7c1fb4295fc6e38d04
Waylon Dixon <wd@signingdaysports.com>
2024-04-25 14:14:40 -0700

    added navbar component

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import Hero from "@/components/main/Hero";
 export default function Home() {
   return (
       <main className="h-full w-full">
-          <div className="flex flex-col gap-20 h-[850px]">
+          <div className="flex flex-col gap-20">
               <Hero />
           </div>
 
