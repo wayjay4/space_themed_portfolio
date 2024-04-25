8b825f180d7b08da1db0ef2450efdf42826c21a2
Waylon Dixon <wd@signingdaysports.com>
2024-04-25 16:30:49 -0700

    added in projects component

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import Hero from "@/components/main/Hero";
 import Skills from "@/components/main/Skills";
 import Encryption from "@/components/main/Encryption";
+import Projects from "@/components/main/Projects";
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
               <Hero />
               <Skills />
               <Encryption />
+              <Projects />
           </div>
       </main>
   );
